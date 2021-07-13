92e09abff0267e9ae50c6226a1e7902049293662
Riya Romanova <riyaromanova13@gmail.com>
2021-07-13 17:24:23 +0300

    смена статуса и неверный номер

--- main.js
+++ main.js
@@ -1,175 +1,203 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", () => {
 
-    const form = document.querySelector('.form'),
-        list = document.querySelector('.list');
+  const form = document.querySelector('.form'),
+    list = document.querySelector('.list');
 
-    let registeredData = new Map(JSON.parse(localStorage.getItem('registeredData')));
+  let registeredData = new Map(JSON.parse(localStorage.getItem('registeredData')));
 
-    const checkEmpty = form => {
-        const inputs = form.querySelectorAll('input'),
-            formBtn = form.querySelector('button');
+  const checkEmpty = form => {
+    const inputs = form.querySelectorAll('input'),
+          formBtn = form.querySelector('button'),
+          warn = document.querySelector('.form__warn');
 
-        let phone = false,
-            name = false,
-            surname = false;
+    let phone = false,
+      name = false,
+      surname = false;
 
-        formBtn.disabled = false;    
-        
-        inputs.forEach(elem => {
-            if (elem.value.trim() === '') {
-                formBtn.disabled = true;
-                elem.classList.add('_error');
-            } else if (elem.classList.contains('_phone')) {
-                if (elem.value.length < 11) {
-					elem.classList.add('_error');
-					phone = false;
-				} else {
-					elem.classList.remove('_error');
-					phone = true;
-				}
-            } else if (elem.classList.contains('_name') || elem.classList.contains('_surname')) {
-				if (elem.value.length < 2) {
-					elem.classList.add('_error');
-					name = surname = false;
-				} else {
-					elem.classList.remove('_error');
-					name = surname = true;
-				}
-			}
-        });
+    formBtn.disabled = false;    
+          
+    inputs.forEach(elem => {
+      if (elem.value.trim() === '') {
+        formBtn.disabled = true;
+        elem.classList.add('_error');
+      } else if (elem.classList.contains('_phone')) {
+        if (elem.value.length < 11) {
+          elem.classList.add('_error');
 
-        if (name === true && surname === true && phone === true) {
-            formBtn.disabled = false;
+          if (!warn) {
+            const wrapper = document.querySelector('.form__wrapper');
+            wrapper.insertAdjacentHTML('beforeend', '<span class="form__warn">неверный формат</span>');
+          }
+          phone = false;
         } else {
-            formBtn.disabled = true;
+          elem.classList.remove('_error');
+          if(warn) {
+            warn.remove();
+          }
+          phone = true;
         }
+      } else if (elem.classList.contains('_name') || elem.classList.contains('_surname')) {
+        if (elem.value.length < 2) {
+          elem.classList.add('_error');
+          name = surname = false;
+        } else {
+          elem.classList.remove('_error');
+          name = surname = true;
+        }
+      }
+    });
+
+    if (name === true && surname === true && phone === true) {
+      formBtn.disabled = false;
+    } else {
+      formBtn.disabled = true;
+    }
 
 
-    };
+  };
 
-    const validateInput = target => {
-        if (target.classList.contains('_phone')) {
+  const validateInput = target => {
+    if (target.classList.contains('_phone')) {
 			target.value = target.value.replace(/[^0-9+]/ig, '');
 		}
-    }
+  }
 
-    const generateKey = () => {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
+  const generateKey = () => {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
 
-    const render = () => {
-        list.textContent = '';
-        const workersList = document.querySelector('.list');
+  const render = () => {
+    list.textContent = '';
+    const workersList = document.querySelector('.list');
 
-        registeredData.forEach(elem => {
-            const li = document.createElement('li');
-            li.classList.add('list__item', 'employee');
-            li.key = elem.key;
+    registeredData.forEach(elem => {
+      const li = document.createElement('li');
+      li.classList.add('list__item', 'employee');
+      li.key = elem.key;
 
-            li.innerHTML = `<div class="employee__wrapper">
-                <div class="employee__name">
-                  <svg class="employee__icon worker">
-                    <use xlink:href="sprite.svg#worker"></use>
-                  </svg>
-                  <span class="employee__text">${elem.name} ${elem.surname}</span>
-                </div>
+      li.innerHTML = `<div class="employee__wrapper">
+          <div class="employee__name">
+            <svg class="employee__icon worker">
+              <use xlink:href="sprite.svg#worker"></use>
+            </svg>
+            <span class="employee__text">${elem.name} ${elem.surname}</span>
+          </div>
 
-                <div class="employee__hidden">
-                  <span class="employee__company">White Rabbit Смоленская</span>
-                  <span class="employee__code">423-000</span>
-                </div>
+          <div class="employee__hidden">
+            <span class="employee__company">White Rabbit Смоленская</span>
+            <span class="employee__code">423-000</span>
+          </div>
 
-                <div class="employee__contact">
-                  <span class="employee__text">${elem.phone}</span>
-                  <svg class="employee__icon edit">
-                    <use xlink:href="sprite.svg#edit"></use>
-                  </svg>
-                </div>
-              </div>
+          <div class="employee__contact">
+            <span class="employee__text">${elem.phone}</span>
+            <svg class="employee__icon edit">
+              <use xlink:href="sprite.svg#edit"></use>
+            </svg>
+          </div>
+        </div>
 
-              <div class="employee__wrapper">
-                <div class="employee__notification">
-                  <svg class="employee__icon sms">
-                    <use xlink:href="sprite.svg#sms"></use>
-                  </svg>
-                  <div class="employee__paragraph">
-                    <span class="employee__text">Необходимо завершить регистрацию по SMS</span>
-                    <span class="employee__text employee__text--link">Повторить SMS с приглашением</span>
-                  </div>
-                </div>
+        <div class="employee__wrapper">
+          <div class="employee__notification">
+            <svg class="employee__icon sms">
+              <use xlink:href="sprite.svg#sms"></use>
+            </svg>
+            <div class="employee__paragraph">
+              <span class="employee__text">Необходимо завершить регистрацию по SMS</span>
+              <span class="employee__text employee__text--link">Повторить SMS с приглашением</span>
+            </div>
+          </div>
 
-                <div class="delete">
-                  <svg class="employee__icon employee__trash trash">
-                    <use xlink:href="sprite.svg#trash"></use>
-                  </svg>
-                </div>
-              </div>`;
-            workersList.append(li);
+          <div class="delete">
+            <svg class="employee__icon employee__trash trash">
+              <use xlink:href="sprite.svg#trash"></use>
+            </svg>
+          </div>
+        </div>`;
+      workersList.append(li);
 
-        });
+    });
 
-        localStorage.setItem('registeredData', JSON.stringify([...registeredData]));
+    localStorage.setItem('registeredData', JSON.stringify([...registeredData]));
 
-    }
+  };
 
-    list.addEventListener('click', event => {
-        const target = event.target;
+  list.addEventListener('click', event => {
+    const target = event.target;
 
-        if (target.closest('.delete')) {
+    if (target.closest('.delete')) {
 
-            const item = target.closest('.list__item'),
-                itemList = document.querySelectorAll('.list__item');
+      const item = target.closest('.list__item'),
+        itemList = document.querySelectorAll('.list__item');
 
-            itemList.forEach(elem => {
-                if (elem.key === item.key) {
-                    elem.classList.add('list__item--deleted');
-                    setTimeout(() => {
-                        elem.remove();
-                    }, 800);
+      itemList.forEach(elem => {
+        if (elem.key === item.key) {
+          elem.classList.add('list__item--deleted');
+          setTimeout(() => {
+              elem.remove();
+          }, 800);
 
-                    for(let key of registeredData.keys()){
-                        console.log(key);
-                        if(key === item.key) {
-                            registeredData.delete(key);
-                            console.log(registeredData);
-                        }
-                    }
-                    localStorage.setItem('registeredData', JSON.stringify([...registeredData]));
-                }
-            });
-            // render();
+          for(let key of registeredData.keys()){
+            if(key === item.key) {
+                registeredData.delete(key);
+                console.log(registeredData);
+            }
+          }
+          localStorage.setItem('registeredData', JSON.stringify([...registeredData]));
         }
-    })
+      });
+    }
 
-    form.addEventListener('input', event => {
-        const target = event.target;
+    if (target.closest('.employee__text--link')) {
+      const item = target.closest('.list__item'),
+        icon = item.querySelector('.sms'),
+        paragraph = item.querySelector('.employee__paragraph'),
+        text = paragraph.querySelector('.employee__text'),
+        link = paragraph.querySelector('.employee__text--link');
 
-        validateInput(target);
-        checkEmpty(form);
+      icon.classList.add('done');
+      icon.innerHTML = `<use xlink:href="sprite.svg#done"></use>`;
+      text.textContent = 'Номер изменен';
+      link.textContent = 'Мы отправили SMS на новый номер телефона';
+      link.classList.remove('employee__text--link');
+      setTimeout(() => {
+        icon.classList.remove('done');
+        icon.innerHTML = `<use xlink:href="sprite.svg#sms"></use>`;
+        text.textContent = 'Необходимо завершить регистрацию по SMS';
+        link.textContent = 'Повторить SMS с приглашением';
+        link.classList.add('employee__text--link')
+      }, 5000);
+    }
+  });
 
-    })
+  form.addEventListener('input', event => {
+    const target = event.target;
 
-    form.addEventListener('submit', event => {
-        event.preventDefault();
+    validateInput(target);
+    checkEmpty(form);
+  });
 
-        const name = form.querySelector('._name').value,
-            surname = form.querySelector('._surname').value,
-            phone = form.querySelector('._phone').value;
+  form.addEventListener('submit', event => {
+    event.preventDefault();
 
-        const newWorker = {
-            name: name,
-            surname: surname,
-            phone: phone,
-            key: generateKey()
-        };
-        registeredData.set(newWorker.key, newWorker);
-        console.log(registeredData);
-        render();
-        form.reset();
-    })
+    const name = form.querySelector('._name').value,
+        surname = form.querySelector('._surname').value,
+        phone = form.querySelector('._phone').value;
 
+    const newWorker = {
+        name: name,
+        surname: surname,
+        phone: phone,
+        key: generateKey()
+    };
 
+    registeredData.set(newWorker.key, newWorker);
+    console.log(registeredData);
     render();
+    form.reset();
+  });
+
+
+  render();
 
 });
